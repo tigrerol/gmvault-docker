c203f6560d34769f61899a93287a08a312a0e2e8
Roland Lechner <roland.lechner@gmail.com>
2020-11-28 13:15:11 +0100

    Deleted Oauth setup, as it is currently disabled by Google. Use app specific password instead.

--- start.sh
+++ start.sh
@@ -40,8 +40,6 @@ echo "${GMVAULT_FULL_SYNC_SCHEDULE} /app/backup_full.sh" >> $CRONTAB
 echo "${GMVAULT_QUICK_SYNC_SCHEDULE} /app/backup_quick.sh" >> $CRONTAB
 
 # Start app.
-if [ -f $OAUTH_TOKEN ]; then
-	echo "Using OAuth token found at $OAUTH_TOKEN."
 
 	if [ "$GMVAULT_SYNC_ON_STARTUP" == "yes" ]; then
 		if [ -d /data/db ]; then
@@ -53,21 +51,8 @@ if [ -f $OAUTH_TOKEN ]; then
 		fi
 	else
 		echo "No sync on startup, see GMVAULT_SYNC_ON_STARTUP if you would like to change this."
-	fi
 
 	crond -f
 fi
-
-echo "#############################"
-echo "#   OAUTH SETUP REQUIRED!   #"
-echo "#############################"
-echo ""
-echo "No Gmail OAuth token found at $OAUTH_TOKEN."
-echo "Please set it up with the following instructions:"
-echo "  1/ Attach a terminal to your container."
-echo "  2/ Run this command:"
-echo "     su -c 'gmvault sync -d /data $GMVAULT_EMAIL_ADDRESS' gmvault"
-echo "  3/ Go to the URL indicated, and copy the token back."
-echo "  4/ Once the synchronization process starts, restart the container."
 
 /bin/bash
